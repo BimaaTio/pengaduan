537048fd544bc74eb002f22bfeba248d68c41662
BimaaTio <bimaatio10@gmail.com>
2023-02-12 11:08:57 +0700

    aa

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -35,7 +35,6 @@
                 </div>
             </div>
             <div class="col-lg-3 col-6">
-
                 <div class="small-box bg-success">
                     <div class="inner">
                         <h3>{{ count($data->where('role', 'm')) }}</h3>
@@ -49,29 +48,15 @@
                 </div>
             </div>
             <div class="col-lg-3 col-6">
-
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3>44</h3>
-                        <p>User Registrations</p>
+                        <h3>{{ count($data->where('role', 'p')) }}</h3>
+                        <p>Data Petugas</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-person-add"></i>
+                        <i class="ion ion-person"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-6">
-
-                <div class="small-box bg-danger">
-                    <div class="inner">
-                        <h3>65</h3>
-                        <p>Unique Visitors</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('petugas') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             @elsecan('member')
